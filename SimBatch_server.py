01af7ffc1f1b12bc215dac5c14382bfc8d9bec40
Markus Wiertarkus <markus@mqs.pl>
2018-06-14 15:40:24 +0100

    FR7

--- SimBatch_server.py
+++ SimBatch_server.py
@@ -1,9 +1,20 @@
+
+simbatch_config_ini = "/job/simbatch_test/config.ini"
+
 import simbatch.core.core as simbatch_core
+
 import simbatch.server.server as simbatch_server
 
-simbatch_config_ini = "S:/simbatch/config.ini"
+# SBS = SimBatchServer( settings ,  Queue( settings ) ,  SimNodes( settings ),  soCo )
+
+
 
 no_gui_batch = simbatch_core.SimBatch("Server", ini_file=simbatch_config_ini)
+no_gui_batch.logger.console_level = 3
+
 
 sim_batch_server = simbatch_server.SimBatchServer(no_gui_batch)
+sim_batch_server.loopsLimit = 4
 sim_batch_server.run()
+
+# print "\n\nAFTER", sim_batch_server.last_info
